1a1390edd60a6fa5d0c525c5f2aeb6dc8f911b59
fahmi ahmad <fahmiahmad@tutanota.com>
2025-07-26 18:30:46 +0700

    chaining middleware

--- cmd/examples/http-middleware/main.go
+++ cmd/examples/http-middleware/main.go
@@ -14,6 +14,38 @@ func middlewareOne(next http.Handler) http.Handler {
 	})
 }
 
+func middlewareTwo(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path, "executing middlewareTwo")
+		next.ServeHTTP(w, r)
+		log.Println(r.URL.Path, "executing middlewareTwo again")
+	})
+}
+
+func middlewareThree(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path, "executing middlewareThree")
+		next.ServeHTTP(w, r)
+		log.Println(r.URL.Path, "executing middlewareThree again")
+	})
+}
+
+func middlewareFour(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path, "executing middlewareFour")
+		next.ServeHTTP(w, r)
+		log.Println(r.URL.Path, "executing middlewareFour again")
+	})
+}
+
+func middlewareFive(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path, "executing middlewareFive")
+		next.ServeHTTP(w, r)
+		log.Println(r.URL.Path, "executing middlewareFive again")
+	})
+}
+
 func fooHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path, "executing fooHandler")
 	w.Write([]byte("foo"))
@@ -27,10 +59,10 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("GET /foo", http.HandlerFunc(fooHandler))
-	mux.Handle("GET /bar", http.HandlerFunc(barHandler))
+	mux.Handle("GET /foo", middlewareThree(middlewareFour(http.HandlerFunc(fooHandler))))
+	mux.Handle("GET /bar", middlewareFour(middlewareFive(http.HandlerFunc(barHandler))))
 
 	log.Print("listening on :3000...")
-	err := http.ListenAndServe(":3000", middlewareOne(mux))
+	err := http.ListenAndServe(":3000", middlewareOne(middlewareTwo(mux)))
 	log.Fatal(err)
 }
