227363c5f3462ce006c7b41cf66a0a0bda841fc5
fahmi ahmad <fahmiahmad@tutanota.com>
2025-07-26 18:27:38 +0700

    middleware

--- cmd/examples/http-middleware/main.go
+++ cmd/examples/http-middleware/main.go
@@ -6,16 +6,29 @@ import (
 	"net/http"
 )
 
-func messageHandler(message string) http.Handler {
+func middlewareOne(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(message))
+		log.Println(r.URL.Path, "executing middlewareOne")
+		next.ServeHTTP(w, r)
+		log.Println(r.URL.Path, "executing middlewareOne again")
 	})
+}
+
+func fooHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path, "executing fooHandler")
+	w.Write([]byte("ok"))
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path, "executing barHandler")
+	w.Write([]byte("ok"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("GET /", messageHandler("hello world"))
+	mux.Handle("GET /foo", http.HandlerFunc(fooHandler))
+	mux.Handle("GET /bar", middlewareOne(http.HandlerFunc(barHandler)))
 
 	log.Print("listening on :3000...")
 	err := http.ListenAndServe(":3000", mux)
